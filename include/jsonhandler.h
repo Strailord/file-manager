dab7ce96c4945c0c1acda059fd1ef4ccc13cea81
Strailord <strailord.boss@gmail.com>
2023-05-27 13:32:46 +0200

    Added cleaning and clearing function

--- include/jsonhandler.h
+++ include/jsonhandler.h
@@ -1,24 +1,24 @@
 #ifndef _JSON_HANDLER_H_
 #define _JSON_HANDLER_H_
-#include "json/json.h"
 #include <string>
 #include <vector>
 #include <experimental/filesystem>
+#include "json/json.h"
 
 class JsonHandler
 {
 private:
 	Json::Value root;
-	int contains(std::string ext);
+	bool contains(std::string ext, std::vector<std::string> &members);
 	std::experimental::filesystem::path config_path = ".";
 public:
+	void clean();
 	void save_config();
 	void load_config();
 	void set_config(std::experimental::filesystem::path);
 	void add(std::string ext, std::string path);
 	int remove(std::string ext);
 	void print_show();
-	Json::Value get_configs();
 };
 
 #endif
